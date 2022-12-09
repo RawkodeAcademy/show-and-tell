98898d6951da0f5c0697a74186613411aaa8ac5d
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2022-12-09 13:42:33 +0100

    [feat] small working sample

---
+++ edgedb-vs-surrealdb/edgedb/README.md
@@ -0,0 +1,11 @@
+# EdgeDB
+
+```shell
+cargo run
+```
+
+## Testing
+
+* `hurl check.hurl` to check all endpoints
+
+## Useful tooling
--- edgedb-vs-surrealdb/edgedb/src/main.rs
+++ edgedb-vs-surrealdb/edgedb/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("EdgeDB is great!");
+use std::net::SocketAddr;
+
+use axum::Router;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", axum::handler::get(|| async { "Hello, World!" }));
+
+    let address = SocketAddr::from(([127, 0, 0, 1], 1337));
+
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
