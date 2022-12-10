a0215b719af93039366286a9ba35a0255b4679c7
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2022-12-10 19:17:14 +0100

    [feat] check for existance of "edgedb" in path

--- edgedb-vs-surrealdb/edgedb/setup.sh
+++ edgedb-vs-surrealdb/edgedb/setup.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+command -v edgedb > /dev/null 2>&1 || {
+    echo "edgedb not found in PATH. Please install from https://www.edgedb.com/docs/intro/cli."
+    exit 1
+}
+
 edgedb project init --non-interactive --server-instance show_and_tell
--- edgedb-vs-surrealdb/edgedb/teardown.sh
+++ edgedb-vs-surrealdb/edgedb/teardown.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-edgedb instance destroy --instance show_and_tell --force
+command -v edgedb > /dev/null 2>&1 || {
+    echo "edgedb not found in PATH. Please install from https://www.edgedb.com/docs/intro/cli."
+    exit 1
+}
+
+edgedb instance destroy --non-interactive --instance show_and_tell --force
