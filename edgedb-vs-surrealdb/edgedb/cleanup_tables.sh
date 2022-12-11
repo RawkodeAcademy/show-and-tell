5f5e64c05dc373db5d8628d94ce7b1d653b20ca1
Stefan Ruzitschka <362487+icepuma@users.noreply.github.com>
2022-12-11 21:45:12 +0100

    [test] more stuff

---
+++ edgedb-vs-surrealdb/edgedb/cleanup_tables.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+SERVER_INSTANCE=${1:-show_and_tell}
+
+command -v edgedb > /dev/null 2>&1 || {
+    echo "edgedb not found in PATH. Please install from https://www.edgedb.com/docs/intro/cli."
+    exit 1
+}
+
+edgedb query --instance $SERVER_INSTANCE 'DELETE default::Repository;'
--- edgedb-vs-surrealdb/edgedb/src/router_tests.rs
+++ edgedb-vs-surrealdb/edgedb/src/router_tests.rs
@@ -3,6 +3,8 @@ use axum_test_helper::TestClient;
 use pretty_assertions::assert_eq;
 use std::process::Command;
 
+use crate::model::Repository;
+
 const EDGEDB_INSTANCE: &str = "router_tests";
 
 #[ctor::ctor]
@@ -35,6 +37,13 @@ fn teardown() {
         .unwrap();
 }
 
+async fn clean_tables() -> anyhow::Result<()> {
+    let client = edgedb_tokio::create_client().await?;
+    client.query_json("DELETE Repository;", &()).await?;
+
+    Ok(())
+}
+
 #[tokio::test]
 async fn test_add_repository() -> anyhow::Result<()> {
     let get_repos_comtrya_comtrya = mockito::mock("GET", "/repos/comtrya/comtrya")
@@ -43,6 +52,7 @@ async fn test_add_repository() -> anyhow::Result<()> {
         .create();
 
     {
+
         let router = crate::router::router().await?;
         let client = TestClient::new(router);
 
@@ -52,6 +62,64 @@ async fn test_add_repository() -> anyhow::Result<()> {
     }
 
     get_repos_comtrya_comtrya.assert();
+
+    clean_tables().await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_list_empty_repositories() -> anyhow::Result<()> {
+    let router = crate::router::router().await?;
+    let client = TestClient::new(router);
+
+    let result = client.get("/repository").send().await;
+
+    assert_eq!(result.status(), StatusCode::OK);
+
+    let repositories = result.json::<Vec<Repository>>().await;
+
+    assert_eq!(repositories, vec![]);
+
+    clean_tables().await?;
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn test_list_repositories() -> anyhow::Result<()> {
+    let get_repos_comtrya_comtrya = mockito::mock("GET", "/repos/comtrya/comtrya")
+        .with_status(200)
+        .with_body_from_file("./fixtures/get_repos_comtrya_comtrya.json")
+        .create();
+    let get_repos_icepuma_fbtoggl = mockito::mock("GET", "/repos/icepuma/fbtoggl")
+        .with_status(200)
+        .with_body_from_file("./fixtures/get_repos_icepuma_fbtoggl.json")
+        .create();
+
+    {
+        let router = crate::router::router().await?;
+        let client = TestClient::new(router);
+
+        let result = client.post("/repository/comtrya/comtrya").send().await;
+        assert_eq!(result.status(), StatusCode::OK);
+
+        let result = client.post("/repository/icepuma/fbtoggl").send().await;
+        assert_eq!(result.status(), StatusCode::OK);
+
+        let result = client.get("/repository").send().await;
+
+        assert_eq!(result.status(), StatusCode::OK);
+
+        let repositories = result.json::<Vec<Repository>>().await;
+
+        assert_eq!(repositories.len(), 2);
+    }
+
+    get_repos_comtrya_comtrya.assert();
+    get_repos_icepuma_fbtoggl.assert();
+
+    clean_tables().await?;
 
     Ok(())
 }
